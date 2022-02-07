ca0ffb61a0fb92c675232330052b27c40a833f45
theartak <kartachok@gmail.com>
2022-02-07 12:34:41 +0400

    Day 3 Task 1 Update code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,10 +8,10 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         byte input = sc.nextByte();
         wideningCasting1(input);
-//        wideningCasting2(input);
-//        wideningCasting3(input);
-//        wideningCasting4(input);
-//        wideningCasting5(input);
+        wideningCasting2(input);
+        wideningCasting3(input);
+        wideningCasting4(input);
+        wideningCasting5(input);
     }
 
     public static void wideningCasting1(byte b) {
@@ -19,23 +19,23 @@ public class Main {
         System.out.println("byte: " + b + ", short: " + a);
     }
 
-//    public static void wideningCasting2(short b) {
-//        int a = b;
-//        System.out.println("short: " + b + ", int: " + a);
-//    }
-//
-//    public static void wideningCasting3(int b) {
-//        long a = b;
-//        System.out.println("int: " + b + ", long: " + a);
-//    }
-//
-//    public static void wideningCasting4(long b) {
-//        float a = b;
-//        System.out.println("long: " + b + ", float: " + a);
-//    }
-//
-//    public static void wideningCasting5(float b) {
-//        double a = b;
-//        System.out.println("float: " + b + ", double: " + a);
-//    }
+    public static void wideningCasting2(short b) {
+        int a = b;
+        System.out.println("short: " + b + ", int: " + a);
+    }
+
+    public static void wideningCasting3(int b) {
+        long a = b;
+        System.out.println("int: " + b + ", long: " + a);
+    }
+
+    public static void wideningCasting4(long b) {
+        float a = b;
+        System.out.println("long: " + b + ", float: " + a);
+    }
+
+    public static void wideningCasting5(float b) {
+        double a = b;
+        System.out.println("float: " + b + ", double: " + a);
+    }
 }
